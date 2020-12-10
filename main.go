f640f2e9fed512a798d97f148d3452819afaa407
Noah Harris <dakoolstwunn@gmail.com>
2020-12-09 20:09:41 -0500

    Use monkelib regex patterns
    Use the regex patterns for nick and email routing

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gastrodon/groudon"
 	"github.com/imonke/monkebase"
+	"github.com/imonke/monkelib"
 
 	"log"
 	"net/http"
@@ -11,8 +12,8 @@ import (
 
 func main() {
 	monkebase.Connect(os.Getenv("MONKEBASE_CONNECTION"))
-	groudon.RegisterHandler("GET", "^/nick/.+/?$", checkNick)
-	groudon.RegisterHandler("GET", "^/email/.+/?$", checkEmail)
+	groudon.RegisterHandler("GET", "^/nick/"+monkelib.NICK_PATTERN+"/?$", checkNick)
+	groudon.RegisterHandler("GET", "^/email/"+monkelib.NICK_PATTERN+"/?$", checkEmail)
 	http.Handle("/", http.HandlerFunc(groudon.Route))
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
